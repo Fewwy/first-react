f6105f829375d6f369fd77b2f081014f6086ef63
Aleksandr Voznesenskii <avoznese@redhat.com>
2022-07-10 14:05:04 +0200

    added pokemon table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import pokemon from './pokemon.json'
+
+console.log(pokemon)
+//structure pokemon = [{-},{-},{-}]
+//pokemon.map((onepokemon) => HTML?????)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Pokemon Table
+      </h1>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Type
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* переключаемся на JS */}
+          {pokemon.map((onepokemon) => (
+            //это уже HTML
+            <tr>
+            <td>
+              {/* мы снова переключаемся на JS */}
+              {onepokemon.name.english}
+            </td>
+            <td>
+              {/* мы снова переключаемся на JS */}
+              {onepokemon.type.join(", ")}
+            </td>
+          </tr>
+          ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
