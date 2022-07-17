843fd7ad4f0c559c7995f8bc07743d58e95000af
Aleksandr Voznesenskii <avoznese@redhat.com>
2022-07-17 14:18:20 +0200

    Added react router and separated project into 2 components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import pokemon from './pokemon.json'
-
-console.log(pokemon)
-//structure pokemon = [{-},{-},{-}]
-//pokemon.map((onepokemon) => HTML?????)
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <div>
-      <h1>
-        Pokemon Table
-      </h1>
-      <table>
-        <thead>
-          <tr>
-            <th>
-              Name
-            </th>
-            <th>
-              Type
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* переключаемся на JS */}
-          {pokemon.map((onepokemon) => (
-            //это уже HTML
-            <tr>
-            <td>
-              {/* мы снова переключаемся на JS */}
-              {onepokemon.name.english}
-            </td>
-            <td>
-              {/* мы снова переключаемся на JS */}
-              {onepokemon.type.join(", ")}
-            </td>
-          </tr>
-          ))
-          }
-        </tbody>
-      </table>
+      <Menu />
+      <p>
+        this is main page
+      </p>
     </div>
   );
 }
